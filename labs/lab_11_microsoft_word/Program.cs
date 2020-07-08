c05c8fd1fff40c92b3761fe0c110f7312363c6fe
Bryn <beatlebryn@gmail.com>
2020-07-08 15:52:16 +0100

    Lab 11 added

---
+++ labs/lab_11_microsoft_word/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xceed.Words.NET;
+
+namespace lab_11_microsoft_word
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create New Word Document
+        
+            var document = DocX.Create("MyReport.docx");
+            document.InsertParagraph("This is an amazing report.");
+            document.InsertParagraph($"Report genereated by <<author>> at {DateTime.Now}");
+            document.Save();
+            Process.Start("WINWORD.EXE", "MyReport.docx");
+
+            
+        }
+    }
+}
