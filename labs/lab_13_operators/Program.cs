0381bde0ff411494fa983a667eadce396c6ebc6b
Bryn <beatlebryn@gmail.com>
2020-07-09 11:38:40 +0100

    lab 13 operators added

---
+++ labs/lab_13_operators/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+
+namespace lab_13_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            int x = 1;
+            int y = x++;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            int a = 1;
+            int b = ++a;
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            */
+
+            /*
+            var averageArrary = new int[] { 2, 5, 1, 8 };
+            double average = averageArrary.Average();
+            Console.WriteLine(average);
+            */
+
+            //var a = 5 / 2;
+            //var b = 5.0 / 2;
+            //var c = 5 / 3;
+
+            /*
+            int days = 26 % 7;
+            int weeks = 26 / 7;
+
+            Console.WriteLine($"Weeks: {weeks} and Days: {days}");
+            */
+
+            /*
+            bool isWearingAParachute = true;
+            bool jumpoutofPlane = false;
+
+            if (isWearingAParachute && jumpoutofPlane)
+            {
+                Console.WriteLine("Jump");
+            }
+            else
+            {
+                Console.WriteLine("Don't Jump");
+            }
+            */
+
+            Console.WriteLine(1^1);
+        }
+    }
+}
