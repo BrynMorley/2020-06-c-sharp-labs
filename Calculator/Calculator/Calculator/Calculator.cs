917969d9a052c4aafccdd56a613fe1be2d63be30
Bryn <beatlebryn@gmail.com>
2020-07-13 11:00:14 +0100

    Calculator added
    still needs tests

---
+++ Calculator/Calculator/Calculator/Calculator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        static public double Add(double num1, double num2)
+        {
+            return Math.Round(num1 + num2);
+        }
+        static public double Subtract(double num1, double num2)
+        {
+            return Math.Round(num1 - num2);
+        }
+
+        static public double Times(double num1, double num2)
+        {
+            return Math.Round(num1 * num2);
+        }
+        static public double Divide(double num1, double num2)
+        {
+            return Math.Round(num1 / num2);
+        }
+        static public double Modulus(double num1, double num2)
+        {
+            return Math.Round(num1 % num2);
+        }
+    }
+}
