5faa93347cffca8641416145ec59ef3c04c9b313
Bryn <beatlebryn@gmail.com>
2020-08-03 12:28:59 +0100

    Added apis

---
+++ SQL/CreateCatDb.sql
@@ -0,0 +1,26 @@
+﻿use master
+
+drop database if exists CatDB
+go
+
+create database CatDB
+go
+
+use CatDB
+go 
+
+drop table Cats
+go
+
+create table Cats (
+CatID int not null identity primary key,
+CatName nvarchar(50),
+Age int ,
+Description nvarchar(50)
+)
+
+Insert into Cats values('Ozzie',15,'Mainecoon')
+Insert into Cats values('Lucy',3,'Sphinx')
+Insert into Cats values('Jasper',1,'Black & White')
+
+select * from Cats
