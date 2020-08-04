6690709563b99648a6715fa124367f5c95da89e6
Bryn <beatlebryn@gmail.com>
2020-08-04 12:32:03 +0100

    Tasks added

---
+++ labs/lab_33_Task_array/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_33_Task_array
+{
+    class Program
+    {
+        static Stopwatch s = new Stopwatch();
+        static void Main(string[] args)
+        {
+            s.Start();
+
+            var taskArray = new Task[5];
+
+            taskArray[0] = Task.Run(
+               () => { Console.WriteLine($"Task 0 completed"); }
+               );
+            taskArray[1] = Task.Run(
+               () => { Console.WriteLine($"Task 1 completed"); }
+               );
+            taskArray[2] = Task.Run(
+               () => { Console.WriteLine($"Task 2 completed"); }
+               );
+            taskArray[3] = Task.Run(
+               () => {
+                 
+                   Console.WriteLine($"Task 3 completed"); }
+               );
+            taskArray[4] = Task.Run(
+               () => {
+                   
+                   Console.WriteLine($"Task 4 completed"); }
+               );
+
+            var oneTask = Task.Run(
+                () =>
+                {
+                  
+                    Console.WriteLine("Individual task completed");
+                }
+                );
+            var getDataBack = Task<string>.Run(
+                () =>
+                {
+                    return "here is some data returned from background task";
+                }
+                );
+
+            Task.WaitAny(taskArray);
+            Console.WriteLine($"First task in array complete at {s.ElapsedMilliseconds}");
+            Task.WaitAll(taskArray);
+            Console.WriteLine($"Task array finished at {s.ElapsedMilliseconds}");
+            oneTask.Wait();
+
+            Console.WriteLine($"Background data returns at {s.ElapsedMilliseconds} - data is {getDataBack.Result} ");
+            Console.WriteLine($"Program ends at {s.ElapsedMilliseconds}");
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ labs/lab_34_task_parallel_processing/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab_34_task_parallel_processing
+{
+    class Program
+    {
+        delegate void myDelegate();
+        
+        static void Main(string[] args)
+        {
+            var s = new Stopwatch();
+            Action instance01 = OvernightTask01;
+            Action instance02 = OvernightTask02;
+            Action instance03 = OvernightTask03;
+            Action instance04 = OvernightTask04;
+            Action instance05 = OvernightTask05;
+
+            Parallel.Invoke(
+                () => {  },
+                () => {  },
+                () => { },
+                () => {  },
+                () => {  },
+                instance01,
+                instance02,
+                instance03,
+                instance04,
+                instance05
+                )
+                ;
+            s.Start();
+
+            var taskArray = new Task[100];
+            for (int i = 0; i < taskArray.Length; i++)
+            {
+                var j = i;
+                taskArray[i] = Task.Run(
+                    () => {
+                        Thread.Sleep(5);
+                        Console.WriteLine($"Task {j} is completed in {s.ElapsedMilliseconds}"); }
+                    );
+            }
+
+
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+
+
+
+        static void OvernightTask01() { Console.WriteLine("Processing overnight task 01"); }
+        static void OvernightTask02() { Console.WriteLine("Processing overnight task 02"); }
+        static void OvernightTask03() { Console.WriteLine("Processing overnight task 03"); }
+        static void OvernightTask04() { Console.WriteLine("Processing overnight task 04"); }
+        static void OvernightTask05() { Console.WriteLine("Processing overnight task 05"); }
+    }
+}
